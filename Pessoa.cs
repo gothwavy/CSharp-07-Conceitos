3854edb9870245307413fea51bd68be41c2d2ab1
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 18:02:08 -0300

    Finalizado✅

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Construtor
+{
+    internal class Pessoa
+    {
+        // Construtor
+        public Pessoa()
+        {
+            Console.WriteLine("Construtor Executado");
+        }
+        public Pessoa(string nome)
+        {
+            Console.WriteLine($"Ola {nome}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _07_Construtor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa Ryan = new Pessoa("Ryan");
+            // A ideia do construtor e simples, assim que um objeto e instanciado o metodo construtor e executado.
+        }
+    }
+}
